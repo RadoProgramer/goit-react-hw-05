abcd5e5e7e1f9ea7ce8c1ee738ff7037eaf6518c
RadoProgramer <adekkubacki@gmail.com>
2024-09-13 16:30:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./components/App/App.jsx";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter basename="/goit-react-hw-05">
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// );
 
 import React from "react";
 import ReactDOM from "react-dom/client";
